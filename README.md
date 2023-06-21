3198c37988dd86442bbcfc5699397c8e80da03a1
Keffrey <keffkahouse@gmail.com>
2023-06-21 19:28:05 +0300

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# THE COCKTAIL COLLECTIONS APP
+An app used to display meals and instructions from [TheCocktailDB website](https://www.thecocktaildb.com/).
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cocktail Collections"),
+      ),
+    );
+  }
+
+}
